4590ed935384df67d113c34a9b648ab1797095b5
D.Ridosh <dridosh@gmail.com>
2022-10-17 16:43:10 +0400

    Use telegram logger service

--- app/Logging/Telegram/TelegramLoggerHandler.php
+++ app/Logging/Telegram/TelegramLoggerHandler.php
@@ -4,6 +4,7 @@
 
     namespace App\Logging\Telegram;
 
+    use App\Services\Telegram\TelegramBotApi;
     use Monolog\Handler\AbstractProcessingHandler;
     use Monolog\Logger;
 
@@ -11,14 +12,23 @@
 
     class TelegramLoggerHandler extends AbstractProcessingHandler
     {
+        private string $token;
+        private int $chat_id;
+
         public function __construct($config)
         {
             $level = Logger::toMonologLevel($config['level']);
             parent::__construct($level);
+            $this->token = $config['token'];
+            $this->chat_id = $config['chat_id'];
         }
 
         protected function write(array $record): void
         {
             //  dd($record['formatted']);
+            TelegramBotApi::sendMessage(
+                $this->token,
+                $this->chat_id,
+                $record['formatted']);
         }
     }
