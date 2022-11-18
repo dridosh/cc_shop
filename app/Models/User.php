ffe6f0ed1094af8221967f4d0a8b3571fed59ca4
D.Ridosh <dridosh@gmail.com>
2022-11-18 18:29:14 +0400

    test AuthController store

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ * @package App\Models
+ * @method static Builder|User query()
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
