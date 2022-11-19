59f82cc78de7a854b6b157a548f504e2fc64252a
D.Ridosh <dridosh@gmail.com>
2022-11-19 06:53:42 +0400

    test Authorize

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -17,6 +17,31 @@
         use RefreshDatabase;
 
 
+        public function test_signup_page_ok(): void
+        {
+            $this->get(action([AuthController::class, 'signUp']))
+                ->assertOk()
+                ->assertSee('Регистрация')
+                ->assertViewIs('auth.sign-up');
+        }
+
+        public function test_login_page_ok(): void
+        {
+            $this->get(action([AuthController::class, 'index']))
+                ->assertOk()
+                ->assertSee('Вход в аккаунт')
+                ->assertViewIs('auth.index');
+        }
+
+        public function test_forgot_page_ok(): void
+        {
+            $this->get(action([AuthController::class, 'forgotPassword']))
+                ->assertOk()
+                ->assertSee('Восстановление пароля')
+                ->assertViewIs('auth.forgot-password');
+        }
+
+
         public function test_creating_user(): void
         {
             Event::fake();
@@ -29,17 +54,23 @@
                 'password_confirmation' => '12345678',
             ];
 
+            $this->assertDatabaseMissing('users', ['email' => $request['email']]);
+
             //   $response = $this->post(route('store'), $request);
             $response = $this->post(action([AuthController::class, 'store']), $request);
-
-            $response->assertRedirect(route('home'));
+            $response->assertValid();
 
             $this->assertDatabaseHas('users', ['email' => $request['email']]);
 
+            $response->assertRedirect(route('home'));
+
+
             $user = User::query()->where(['email' => $request['email']])->first();
+
 
             Event::assertDispatched(Registered::class);
             Event::assertListening(Registered::class, SendEmailNewUserListener::class);
+
             $event = new Registered($user);
             $listener = new SendEmailNewUserListener();
             $listener->handle($event);
@@ -48,8 +79,37 @@
 
             $response->assertRedirect(route('home'));
 
-
             $this->assertAuthenticatedAs($user);
+        }
+
+        public function test_signIn_success(): void
+        {
+            $password = '12345679';
+            $user = User::factory()->create(([
+                'name'     => 'Name',
+                'email'    => 'rt@mail.ru',
+                'password' => bcrypt($password),
+            ]));
+            $request = [
+                'email'    => $user->email,
+                'password' => $password,
+            ];
+            $response = $this->post(action([AuthController::class, 'signIn']), $request);
+            $response->assertValid()
+                ->assertRedirect(route('home'));
+            $this->assertAuthenticatedAs($user);
+        }
+
+        public function test_logout_success(): void
+        {
+            $user = User::factory()->create(([
+                'email' => 'rt@mail.ru',
+            ]));
+
+            $this->actingAs($user)
+                ->delete(action([AuthController::class, 'logOut']))
+                ->assertRedirect(route('home'));
+            $this->assertGuest();
         }
 
     }
